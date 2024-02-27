2d8d9df02589703ee07dbde4851ea63652e4994a
Grace Gallivan <gracee.gallivan@gmail.com>
2024-02-27 12:10:04 -0800

    completed second query

--- queries.sql
+++ queries.sql
@@ -3,3 +3,10 @@ SELECT *
     FROM owners
     FULL OUTER JOIN vehicles
     ON (owners.id = vehicles.owner_id);
+
+SELECT first_name, COUNT(owner_id)
+    FROM vehicles
+        INNER JOIN owners
+            ON owners.id = vehicles.owner_id
+    GROUP BY first_name
+    ORDER BY first_name;
